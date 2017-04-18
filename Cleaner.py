d83d5bbf3e6b2d771c873b8715fb5dc4a9fe0ada
Mario Alina <Mario.alina11@gmail.com>
2017-04-18 23:22:18 +0200

    Update Cleaner.py

--- Cleaner.py
+++ Cleaner.py
@@ -12,18 +12,15 @@
 # python Cleaner.py PATH 
 # PATH is a starting directory , the script should delete all the compressed files,
 # in the directory and ALL the subdirectories so be carefull.
-
 import os
 import magic
 
 print "Please enter a correct Starting Path."
-print "All the compressed files within that directory, 
-print "and also all the subdirectories, will be deleted."
+print "All the .rar files within that directory and also deeper, will be deleted."
 PATH = raw_input("> ")
 
 list_of_extentions = ["7z","ar","rar","cbz","tar.gz","tgz","tar.Z"
 "tar.bz2","tbz2","tar.lzma","zip","zipx"]
- 
 deleted_count = 0
 total_byte_size = 0 
 
@@ -32,17 +29,17 @@ for dirpath, dirname ,filename in os.walk(PATH):
         with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
             file_path = dirpath + "/" + file
             a_file = m.id_filename(file_path)
-            print a_file
             for extention in list_of_extentions:
-                
                 if a_file.endswith(extention):
                     total_byte_size += os.stat(file_path).st_size
+                    print "Deleting.. --> " + file
                     os.remove(file_path)
                     deleted_count += 1
 
 total_mb_size = total_byte_size / 1048576
 total_gb_size = float(total_mb_size) / 1000.0
 msg = "Done!, {0} file(s) deleted ,{1}MB / {2}GB of space freed"
+
 if deleted_count != 0:
     print msg.format(deleted_count,total_mb_size,float(total_gb_size))
 else:
