edd0b45368fad7efee823bc3b8c2674cfb53e411
Mario Alina <Mario.alina11@gmail.com>
2017-04-18 22:41:35 +0200

    Update Cleaner.py

--- Cleaner.py
+++ Cleaner.py
@@ -1,6 +1,6 @@
-# This is a script for deleting the compressed files.(right now , only works for .rar extentions)
+# This is a script for deleting the compressed files.
 # Using a magic library to determine if the files is indeed compressed.
-
+# Works with some common extentions , feel free to add more.
 import os
 import magic
 
@@ -8,24 +8,29 @@ print "Please enter a correct Starting Path."
 print "All the .rar files within that directory and also deeper, will be deleted."
 PATH = raw_input("> ")
 
-#file_type = "" # later I'll make a list of all the compressed extensions
+list_of_extentions = ["7z","ar","rar","cbz","tar.gz","tgz","tar.Z"
+"tar.bz2","tbz2","tar.lzma","zip","zipx"]
+ 
 deleted_count = 0
-total_byte_size = 0 #bytes
-
+total_byte_size = 0 
 
 for dirpath, dirname ,filename in os.walk(PATH):
     for file in filename:
         with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
-            file_path = dirpath + "/" + file #LOL
-            a_file = m.id_filename(file_path) 
-            if a_file.endswith("rar"):
-                total_byte_size += os.stat(file_path).st_size
-                os.remove(file_path)
-                deleted_count += 1
+            file_path = dirpath + "/" + file
+            a_file = m.id_filename(file_path)
+            print a_file
+            for extention in list_of_extentions:
+                
+                if a_file.endswith(extention):
+                    total_byte_size += os.stat(file_path).st_size
+                    os.remove(file_path)
+                    deleted_count += 1
 
 total_mb_size = total_byte_size / 1048576
+total_gb_size = float(total_mb_size) / 1000.0
 
 if deleted_count != 0:
-    print "Done!, {0} files deleted ,{1} MB of space freed".format(deleted_count,total_mb_size)
+    print "Done!, {0} file(s) deleted ,{1}MB / {2}GB of space freed".format(deleted_count,total_mb_size,float(total_gb_size))
 else:
     print "No such files found."
